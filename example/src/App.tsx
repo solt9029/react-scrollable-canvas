76799f56a12875e6358d4ec49a5fb014c4ae8256
Kenshi Shiode <atemu1212@yahoo.co.jp>
2019-12-18 21:42:43 +0900

    Add Jumbotron

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link, Route } from 'react-router-dom';
 import Navbar from './Navbar';
+import Jumbotron from './Jumbotron';
 
 const Home = () => <h2>Home</h2>;
 const About = () => <h2>About</h2>;
@@ -9,6 +10,8 @@ const App = () => {
   return (
     <div>
       <Navbar />
+      <Jumbotron />
+
       <Link to="/">Home</Link>
       <Link to="/about">About</Link>
       <Route exact path="/" component={Home} />
