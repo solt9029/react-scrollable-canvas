a9ba040c1b95d48fde95b503e76a5c87fb99e7ca
Kenshi Shiode <atemu1212@yahoo.co.jp>
2019-12-19 22:25:15 +0900

    Add canvas

--- example/src/ResizableCanvas.tsx
+++ example/src/ResizableCanvas.tsx
@@ -1,25 +1,52 @@
-import React, { Component } from 'react';
+import React, { Component, createRef } from 'react';
 import { throttle } from 'lodash';
 import { ScrollableCanvas } from './react-scrollable-canvas';
 
 export default class ResizableCanvas extends Component<{}, { width: number }> {
+  private canvasRef = createRef<HTMLCanvasElement>();
+  private ctx: CanvasRenderingContext2D | null = null;
+  private scrollTop = 0;
+  private scrollLeft = 0;
   state = {
-    width: 0,
+    width: 300,
   };
 
   componentDidMount() {
     window.addEventListener('resize', this.onResize);
     this.setState({ width: window.innerWidth });
+
+    if (this.canvasRef.current === null) {
+      return;
+    }
+    this.ctx = this.canvasRef.current.getContext('2d');
+  }
+
+  componentDidUpdate() {
+    this.draw(this.scrollTop, this.scrollLeft);
   }
 
   componentWillUnmount() {
     window.removeEventListener('resize', this.onResize);
   }
 
-  onResize = throttle(() => {
+  private onResize = throttle(() => {
     this.setState({ width: window.innerWidth });
-    console.log(this.state.width);
   }, 10);
+
+  private draw = (scrollTop: number, scrollLeft: number) => {
+    if (this.ctx === null) {
+      return;
+    }
+    this.ctx.clearRect(0, 0, 300, 300);
+    this.ctx.fillRect(0 - scrollLeft, 190 - scrollTop, 30, 30);
+    this.ctx.fillRect(200 - scrollLeft, 190 - scrollTop, 30, 30);
+  };
+
+  private onScroll = (scrollTop: number, scrollLeft: number) => {
+    this.draw(scrollTop, scrollLeft);
+    this.scrollTop = scrollTop;
+    this.scrollLeft = scrollLeft;
+  };
 
   render() {
     return (
@@ -28,6 +55,8 @@ export default class ResizableCanvas extends Component<{}, { width: number }> {
         height={this.state.width}
         largeWidth={this.state.width * 2}
         largeHeight={this.state.width * 2}
+        canvasRef={this.canvasRef}
+        onScroll={this.onScroll}
       />
     );
   }
