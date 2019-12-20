49594c498f96ff79fbb0d334ce0527fb951597fc
Kenshi Shiode <atemu1212@yahoo.co.jp>
2019-12-20 21:30:32 +0900

    Add constants

--- example/src/ScrollableCanvasExample.tsx
+++ example/src/ScrollableCanvasExample.tsx
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 import { ScrollableCanvas } from './react-scrollable-canvas';
 
+const WIDTH = 300;
+const HEIGHT = 300;
+const LARGE_WIDTH = 600;
+const LARGE_HEIGHT = 600;
+const CIRCLE_RADIUS = 5;
+const CIRCLE_SIZE = 30;
+
 export default class ScrollableCanvasExample extends Component {
   private canvasRef = React.createRef<HTMLCanvasElement>();
   private ctx: CanvasRenderingContext2D | null = null;
@@ -9,9 +16,16 @@ export default class ScrollableCanvasExample extends Component {
     if (this.ctx === null) {
       return;
     }
-    this.ctx.clearRect(0, 0, 300, 300);
-    this.ctx.fillRect(0 - scrollLeft, 190 - scrollTop, 30, 30);
-    this.ctx.fillRect(200 - scrollLeft, 190 - scrollTop, 30, 30);
+    this.ctx.clearRect(0, 0, WIDTH, HEIGHT);
+
+    for (let y = -scrollTop % CIRCLE_SIZE; y < HEIGHT - (scrollTop % CIRCLE_SIZE); y += CIRCLE_SIZE) {
+      for (let x = -scrollLeft % CIRCLE_SIZE; x < WIDTH - (scrollLeft % CIRCLE_SIZE); x += CIRCLE_SIZE) {
+        this.ctx.beginPath();
+        this.ctx.arc(x + CIRCLE_SIZE / 2, y + CIRCLE_SIZE / 2, CIRCLE_RADIUS, 0, 360, false);
+        this.ctx.fillStyle = `rgba(${(scrollLeft + x) / 2}, ${(scrollTop + y) / 2}, 128, 0.8)`;
+        this.ctx.fill();
+      }
+    }
   };
 
   private onScroll = (scrollTop: number, scrollLeft: number) => {
@@ -30,13 +44,13 @@ export default class ScrollableCanvasExample extends Component {
     return (
       <div>
         <ScrollableCanvas
-          width={300}
-          height={300}
-          largeWidth={600}
-          largeHeight={600}
+          width={WIDTH}
+          height={HEIGHT}
+          largeWidth={LARGE_WIDTH}
+          largeHeight={LARGE_HEIGHT}
           canvasRef={this.canvasRef}
           onScroll={this.onScroll}
-          wait={10}
+          wait={1}
         />
       </div>
     );
