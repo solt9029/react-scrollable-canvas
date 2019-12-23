a766c233ec6452ec8043abcddea55c6e101ce8fb
Kenshi Shiode <atemu1212@yahoo.co.jp>
2019-12-23 23:16:24 +0900

    Add container

--- example/src/ExamplesPage.tsx
+++ example/src/ExamplesPage.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import Navbar from './Navbar';
+import { Container as RCContainer } from 'reactstrap';
+import styled from 'styled-components';
+
+const Container = styled(RCContainer)`
+  margin-top: 30px;
+`;
 
 const ExamplesPage = () => {
   return (
     <>
       <Navbar active="examples" />
+      <Container>text</Container>
     </>
   );
 };
